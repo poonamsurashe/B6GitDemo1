ebd0d4d0e2812a0bce19c6ee8b8c43004fda6ae8
unknown <poonamsurashe0@gmail.com>
2023-03-16 22:24:59 +0530

    Added ArrayList

---
+++ B6collection/src/com/b6collection/Arraylist1.java
@@ -0,0 +1,12 @@
+package com.b6collection;
+
+import java.util.ArrayList;
+
+public class Arraylist1 {
+public static void main(String[] args) {
+	ArrayList al= new ArrayList();
+	al.add(10);
+	al.add("Sonali");
+	System.out.println(al);
+}
+}
